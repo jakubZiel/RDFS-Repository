46b2adf8277e393a0beeb0b02e8b85ae640b7611
Jakub  Zieliński <jzielins@mion.elka.pw.edu.pl>
2022-05-07 10:41:23 +0200

    init

---
+++ src/main/java/com/rdfsonto/rdfsonto/RdfsOntoApplication.java
@@ -0,0 +1,13 @@
+package com.rdfsonto.rdfsonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdfsOntoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdfsOntoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rdfsonto/rdfsonto/RdfsOntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdfsonto.rdfsonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdfsOntoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
