63b9c69a363dae72f43fdbd119e2b5e44c117825
Jakub <=>
2022-12-17 12:14:37 +0100

    change security config

--- src/main/java/com/rdfsonto/rdfsonto/config/security/SecurityConfig.java
+++ src/main/java/com/rdfsonto/rdfsonto/config/security/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @KeycloakConfiguration
@@ -26,16 +32,26 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
+    @Bean
+    public FilterRegistrationBean corsFilter()
+    {
+        final var source = new UrlBasedCorsConfigurationSource();
+        final var config = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        final var bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception
     {
         super.configure(http);
 
-        http.cors().and().csrf().disable();
+        http.addFilter(corsFilter().getFilter());
+        http.csrf().disable();
 
         http.authorizeRequests()
-            .antMatchers("/neo4j/*")
-            .hasRole("USER")
             .anyRequest()
             .permitAll();
     }
