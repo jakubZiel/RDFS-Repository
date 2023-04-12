fdef6c961d5c52d32f6447816ba91cb59c6a6fdd
Jakub <=>
2023-04-12 02:42:46 +0200

    refactor usage of query templates

--- src/main/java/com/rdfsonto/classnode/database/ClassNodeNeo4jDriverRepository.java
+++ src/main/java/com/rdfsonto/classnode/database/ClassNodeNeo4jDriverRepository.java
@@ -1,5 +1,23 @@
 package com.rdfsonto.classnode.database;
 
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.AND;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.CLEAR_PROPERTIES_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.CREATE_NODE_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.FIND_ALL_NODE_PROPERTIES_QUERY_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.INCOMING_NEIGHBOURS_QUERY_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.MATCH_NODE_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.NEIGHBOUR_RECORD_KEY;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.NODE_IDS_KEY;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.NODE_ID_KEY;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.NODE_KEY;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.OUTGOING_NEIGHBOURS_QUERY_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.RELATIONSHIP_ID_KEY;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.RELATION_RECORD_KEY;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.SET_NODE_PROPERTIES_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.SET_PROPERTY_TEMPLATE;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.SOURCE_NODE_ID_RECORD_KEY;
+import static com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepositoryTemplates.URI_KEY;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,7 +33,6 @@ import org.neo4j.driver.exceptions.Neo4jException;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Repository;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rdfsonto.classnode.service.ClassNode;
 import com.rdfsonto.classnode.service.FilterCondition;
 
@@ -28,49 +45,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ClassNodeNeo4jDriverRepository
 {
-    private static final String MATCH_NODE_TEMPLATE = """
-        MATCH (node:Resource) where id(node) = $nodeId
-        """;
-    private static final String CREATE_NODE_TEMPLATE = """
-        CREATE (node:Resource{uri: $uri}) return node
-        """;
-    private static final String CLEAR_PROPERTIES_TEMPLATE = """
-        node = {uri: $uri}
-        """;
-
-    private static final String SET_NODE_PROPERTIES_TEMPLATE = """
-        MATCH (node) where id(node) = %s
-        SET""";
-    private static final String SET_PROPERTY_TEMPLATE = """
-        node.`%s` = $param%s""";
-
-    private static final String OUTGOING_NEIGHBOURS_QUERY_TEMPLATE = """
-        MATCH (n:Resource)-[rel]->(neighbour:Resource)
-        WHERE id(n) IN $nodeIds
-        RETURN neighbour, id(n) as source, type(rel) as relation, id(rel) as relationshipId
-        """;
-
-    private static final String INCOMING_NEIGHBOURS_QUERY_TEMPLATE = """
-        MATCH (n:Resource)<-[rel]-(neighbour:Resource)
-        WHERE id(n) IN $nodeIds
-        RETURN neighbour, id(n) as source, type(rel) as relation, id(rel) as relationshipId
-        """;
-
-    private static final String FIND_ALL_NODE_PROPERTIES_QUERY_TEMPLATE = """
-        UNWIND $nodeIds AS nodeId
-        MATCH (n:Resource) WHERE id(n) = nodeId
-        RETURN id(n) as id, properties(n) as properties
-        """;
-
-    private static final String NEIGHBOUR_RECORD_KEY = "neighbour";
-    private static final String RELATION_RECORD_KEY = "relation";
-    private static final String SOURCE_NODE_ID_RECORD_KEY = "source";
-    private static final String NODE_KEY = "node";
-    private static final String NODE_IDS_KEY = "nodeIds";
-    private static final String NODE_ID_KEY = "nodeId";
-    private static final String URI_KEY = "uri";
-    private static final String RELATIONSHIP_ID_KEY = "relationshipId";
-    private static final String AND = "AND";
 
     private final Driver driver;
     private final RelationshipNeo4jDriverRepository relationshipNeo4jDriverRepository;
@@ -105,7 +79,6 @@ public class ClassNodeNeo4jDriverRepository
 
             transaction.commit();
 
-            // TODO make it inside commit
             return classNodeRepository.findProjectionById(nodeId)
                 .orElseThrow(() -> new IllegalStateException("Class node with ID: %s is not found after after being saved.".formatted(nodeId)));
         }
@@ -138,7 +111,7 @@ public class ClassNodeNeo4jDriverRepository
     {
         try (final var session = driver.session())
         {
-            final var paramMap = Map.of("nodeIds", (Object) ids);
+            final var paramMap = Map.of(NODE_IDS_KEY, (Object) ids);
             final var queryResult = session.run(FIND_ALL_NODE_PROPERTIES_QUERY_TEMPLATE, paramMap);
 
             return queryResult.stream()
@@ -175,7 +148,8 @@ public class ClassNodeNeo4jDriverRepository
     private List<ClassNodeVo> findNeighbours(final List<Long> ids, final RelationshipDirection relationshipDirection)
     {
         final var query = relationshipDirection == RelationshipDirection.INCOMING ?
-            INCOMING_NEIGHBOURS_QUERY_TEMPLATE : OUTGOING_NEIGHBOURS_QUERY_TEMPLATE;
+            INCOMING_NEIGHBOURS_QUERY_TEMPLATE :
+            OUTGOING_NEIGHBOURS_QUERY_TEMPLATE;
 
         if (ids == null || ids.isEmpty())
         {
@@ -207,28 +181,27 @@ public class ClassNodeNeo4jDriverRepository
             return "MATCH (node:Resource)\n";
         }
 
-        final var concatenatedLabels = labels.stream().reduce("", (matchStatement, label) -> matchStatement + ":%s".formatted(label));
-        return "MATCH (node:Resource%s)\n".formatted(concatenatedLabels);
+        final var concatenatedLabels = labels.stream()
+            .map("`%s`"::formatted)
+            .collect(Collectors.joining(":"));
+
+        return "MATCH (node:Resource:%s)\n".formatted(concatenatedLabels);
     }
 
     private String buildWhereClause(final List<FilterCondition> propertyFilters)
     {
         if (propertyFilters.isEmpty())
         {
-            return "";
+            return Strings.EMPTY;
         }
 
-        final var whereClause = new StringBuilder("WHERE ");
+        final var whereClause = "WHERE ";
 
-        propertyFilters.forEach(filter -> {
-            final var condition = "node.`%s` %s '%s' %s".formatted(filter.property(), filter.operator(), filter.value(), AND);
-            whereClause.append("%s \n".formatted(condition));
-        });
+        final var propertiesConditionStatement = propertyFilters.stream()
+            .map(filter -> "node.`%s` %s '%s'".formatted(filter.property(), filter.operator(), filter.value()))
+            .collect(Collectors.joining(AND + Strings.LINE_SEPARATOR));
 
-        final int unusedAndIndex = whereClause.lastIndexOf(AND);
-        whereClause.delete(unusedAndIndex, unusedAndIndex + AND.length());
-
-        return whereClause.toString();
+        return String.join(Strings.LINE_SEPARATOR, List.of(whereClause, propertiesConditionStatement));
     }
 
     private void handleRelationshipDiff(final long nodeId,
@@ -263,9 +236,9 @@ public class ClassNodeNeo4jDriverRepository
     {
         final var uriParam = Map.of(URI_KEY, (Object) nodeUpdate.uri());
 
-        final var clearPropsStatement = Strings.join(
+        final var clearPropsStatement = String.join(Strings.LINE_SEPARATOR,
             List.of(SET_NODE_PROPERTIES_TEMPLATE.formatted(nodeId),
-                CLEAR_PROPERTIES_TEMPLATE), '\n');
+                CLEAR_PROPERTIES_TEMPLATE));
 
         final var clearPropsQuery = new Query(clearPropsStatement, uriParam);
         transaction.run(clearPropsQuery);
@@ -284,8 +257,9 @@ public class ClassNodeNeo4jDriverRepository
             .toList();
 
         final var setPropertiesStatement = Strings.join(setPropertiesComponents, ',');
-        final var matchAndSetPropertiesComponents = List.of(SET_NODE_PROPERTIES_TEMPLATE.formatted(nodeId), setPropertiesStatement);
-        final var matchAndSetPropertiesStatement = Strings.join(matchAndSetPropertiesComponents, '\n');
+        final var matchAndSetPropertiesComponents =
+            List.of(SET_NODE_PROPERTIES_TEMPLATE.formatted(nodeId), setPropertiesStatement);
+        final var matchAndSetPropertiesStatement = String.join(Strings.LINE_SEPARATOR, matchAndSetPropertiesComponents);
 
         final var paramMap = IntStream.range(0, nodeUpdate.properties().size())
             .mapToObj(index -> Pair.of("param" + index, properties.get(index).getSecond()))
@@ -299,7 +273,8 @@ public class ClassNodeNeo4jDriverRepository
     private void handleLabelsDiff(final ClassNode nodeUpdate, final long nodeId, final Transaction transaction)
     {
         final Map<String, Object> nodeIdParamMap = Map.of(NODE_ID_KEY, nodeId);
-        final var queryNodeLabels = new Query(MATCH_NODE_TEMPLATE + "return labels(node) as labels", nodeIdParamMap);
+        final var queryNodeLabels =
+            new Query(MATCH_NODE_TEMPLATE + "return labels(node) as labels", nodeIdParamMap);
 
         final var persistedLabels = transaction.run(queryNodeLabels).single().get("labels").asList(Value::asString).stream()
             .map("`%s`"::formatted)
@@ -314,7 +289,7 @@ public class ClassNodeNeo4jDriverRepository
         final var removeQuery = "REMOVE node:" + Strings.join(persistedLabels, ':');
 
         final var updateLabelsQueryComponents = List.of(MATCH_NODE_TEMPLATE, removeQuery, setQuery);
-        final var updateLabelsQuery = Strings.join(updateLabelsQueryComponents, '\n');
+        final var updateLabelsQuery = String.join(Strings.LINE_SEPARATOR, updateLabelsQueryComponents);
 
         final var query = new Query(updateLabelsQuery, nodeIdParamMap);
 
---
+++ src/main/java/com/rdfsonto/classnode/database/ClassNodeNeo4jDriverRepositoryTemplates.java
@@ -0,0 +1,43 @@
+package com.rdfsonto.classnode.database;
+
+public class ClassNodeNeo4jDriverRepositoryTemplates
+{
+    static final String MATCH_NODE_TEMPLATE = """
+        MATCH (node:Resource) where id(node) = $nodeId
+        """;
+    static final String CREATE_NODE_TEMPLATE = """
+        CREATE (node:Resource{uri: $uri}) return node
+        """;
+    static final String CLEAR_PROPERTIES_TEMPLATE = """
+        node = {uri: $uri}
+        """;
+    static final String SET_NODE_PROPERTIES_TEMPLATE = """
+        MATCH (node) where id(node) = %s
+        SET""";
+    static final String SET_PROPERTY_TEMPLATE = """
+        node.`%s` = $param%s""";
+    static final String OUTGOING_NEIGHBOURS_QUERY_TEMPLATE = """
+        MATCH (n:Resource)-[rel]->(neighbour:Resource)
+        WHERE id(n) IN $nodeIds
+        RETURN neighbour, id(n) as source, type(rel) as relation, id(rel) as relationshipId
+        """;
+    static final String INCOMING_NEIGHBOURS_QUERY_TEMPLATE = """
+        MATCH (n:Resource)<-[rel]-(neighbour:Resource)
+        WHERE id(n) IN $nodeIds
+        RETURN neighbour, id(n) as source, type(rel) as relation, id(rel) as relationshipId
+        """;
+    static final String FIND_ALL_NODE_PROPERTIES_QUERY_TEMPLATE = """
+        UNWIND $nodeIds AS nodeId
+        MATCH (n:Resource) WHERE id(n) = nodeId
+        RETURN id(n) as id, properties(n) as properties
+        """;
+    static final String NEIGHBOUR_RECORD_KEY = "neighbour";
+    static final String RELATION_RECORD_KEY = "relation";
+    static final String RELATIONSHIP_ID_KEY = "relationshipId";
+    static final String SOURCE_NODE_ID_RECORD_KEY = "source";
+    static final String NODE_KEY = "node";
+    static final String NODE_IDS_KEY = "nodeIds";
+    static final String NODE_ID_KEY = "nodeId";
+    static final String URI_KEY = "uri";
+    static final String AND = "AND";
+}
