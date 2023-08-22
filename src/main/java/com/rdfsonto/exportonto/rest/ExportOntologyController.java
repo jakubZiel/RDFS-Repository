2fbb472b3e359ff5db70611bfdfc03db6825de1a
Jakub <=>
2023-08-23 00:40:17 +0200

    export handling - remove dead code

--- src/main/java/com/rdfsonto/exportonto/rest/ExportOntologyController.java
+++ src/main/java/com/rdfsonto/exportonto/rest/ExportOntologyController.java
@@ -2,39 +2,23 @@ package com.rdfsonto.exportonto.rest;
 
 import static com.rdfsonto.classnode.service.ClassNodeExceptionErrorCode.INVALID_REQUEST;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rdfsonto.classnode.service.ClassNodeException;
 import com.rdfsonto.classnode.service.ClassNodeExceptionErrorCode;
 import com.rdfsonto.exportonto.service.ExportOntologyService;
-import com.rdfsonto.infrastructure.workspacemanagement.WorkspaceManagementService;
 import com.rdfsonto.rdf4j.RdfFormatParser;
 
 import lombok.RequiredArgsConstructor;
@@ -50,7 +34,7 @@ public class ExportOntologyController
     private final ExportOntologyService exportOntologyService;
 
     @PostMapping
-    public ResponseEntity<?> exportRdfResource(@RequestBody final ExportOntologyRequest exportOntologyRequest, final HttpServletResponse response)
+    public ResponseEntity<?> exportRdfResource(@RequestBody final ExportOntologyRequest exportOntologyRequest)
     {
         if (isInvalid(exportOntologyRequest))
         {
@@ -59,29 +43,9 @@ public class ExportOntologyController
         }
 
         final var rdfFormat = RdfFormatParser.parse(exportOntologyRequest.rdfFormat());
-
         final var ontologyExport = exportOntologyService.exportOntology(exportOntologyRequest.userId(), exportOntologyRequest.projectId(), rdfFormat);
 
-        try
-        {
-            if (false)
-            {
-                throw new IOException();
-            }
-            /*attachFileToHttpResponse(response,
-                rdfFormat,
-                ontologyExport.exportedOntologyFile(),
-                exportOntologyRequest.fileName(),
-                ontologyExport.inputStream());
-            workspaceManagementService.clearWorkspace(ontologyExport.exportId());*/
-            return ResponseEntity.ok(exportOntologyRequest.fileName());
-
-        }
-        catch (final IOException ioException)
-        {
-            log.error("Error attaching a file to export ID: {} to http response", ontologyExport.exportId());
-            return ResponseEntity.internalServerError().build();
-        }
+        return ResponseEntity.ok(ontologyExport.exportedOntologyFile().getName());
     }
 
     @GetMapping(value = "/file/{userId}/{projectId}")
@@ -108,7 +72,7 @@ public class ExportOntologyController
 
         if (classNodeException.getErrorCode() == ClassNodeExceptionErrorCode.INTERNAL_ERROR)
         {
-            classNodeException.printStackTrace();
+            log.error("Class node exception", classNodeException);
             return ResponseEntity.internalServerError().build();
         }
 
--- src/main/java/com/rdfsonto/exportonto/service/ExportOntologyServiceImpl.java
+++ src/main/java/com/rdfsonto/exportonto/service/ExportOntologyServiceImpl.java
@@ -4,41 +4,28 @@ import static com.rdfsonto.classnode.service.ClassNodeExceptionErrorCode.INVALID
 import static com.rdfsonto.classnode.service.ClassNodeExceptionErrorCode.INVALID_USER_ID;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.stream.IntStream;
 import java.util.zip.GZIPOutputStream;
 
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.rdfsonto.classnode.database.ClassNodeNeo4jDriverRepository;
-import com.rdfsonto.classnode.database.ClassNodeRepository;
 import com.rdfsonto.classnode.service.ClassNodeException;
-import com.rdfsonto.classnode.service.ClassNodeService;
 import com.rdfsonto.classnode.service.UniqueUriIdHandler;
 import com.rdfsonto.classnode.service.UriRemoveUniquenessHandler;
 import com.rdfsonto.classnode.service.UriUniquenessHandler;
-import com.rdfsonto.exportonto.database.ExportRepository;
 import com.rdfsonto.infrastructure.workspacemanagement.WorkspaceManagementService;
-import com.rdfsonto.prefix.service.PrefixNodeService;
 import com.rdfsonto.project.database.ProjectRepository;
 import com.rdfsonto.project.service.ProjectService;
 import com.rdfsonto.user.service.UserService;
@@ -53,22 +40,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 class ExportOntologyServiceImpl implements ExportOntologyService
 {
-    private final int BATCH_SIZE = 100_000;
-    private static final long MAX_NODE_COUNT = 1000;
-    private static final String PROJECT_LABEL_ROOT = "http://www.user_neo4j.com#";
-
     @Value("${rdf4j.downloader.workspace}")
     private String WORKSPACE_DIR;
     private final ProjectService projectService;
     private final UserService userService;
-    private final PrefixNodeService prefixNodeService;
-    private final ClassNodeRepository classNodeRepository;
-    private final ClassNodeNeo4jDriverRepository classNodeNeo4jDriverRepository;
-    private final Neo4jGraphSerializer neo4JGraphSerializer;
-    private final RDFExporter rdfExporter;
     private final UniqueUriIdHandler uniqueUriIdHandler;
-    private final ExportRepository exportRepository;
-    private final ClassNodeService classNodeService;
     private final UriUniquenessHandler uriUniquenessHandler;
     private final UriRemoveUniquenessHandler uriRemoveUniquenessHandler;
     private final ProjectRepository projectRepository;
@@ -91,9 +67,9 @@ class ExportOntologyServiceImpl implements ExportOntologyService
         try
         {
             final var extractedOntology = extractOntology(userId, projectId, rdfFormat);
-            final var exportedOntology = exportOntology(userId, projectId, extractedOntology);
+            final var compressedFile = prepareCompressedFile(extractedOntology);
 
-            final var snapshotName = exportedOntology.exportedOntologyFile().getName();
+            final var snapshotName = compressedFile.exportedOntologyFile().getName();
             final var snapshotTime = System.currentTimeMillis();
 
             projectRepository.saveSnapshot(projectId, snapshotTime, snapshotName);
@@ -101,7 +77,7 @@ class ExportOntologyServiceImpl implements ExportOntologyService
             final var oldSnapshotFile = project.getSnapshotFile();
             workspaceManagementService.clearWorkspace(oldSnapshotFile);
 
-            return exportedOntology;
+            return compressedFile;
         }
         catch (final IOException | InterruptedException exception)
         {
@@ -148,143 +124,44 @@ class ExportOntologyServiceImpl implements ExportOntologyService
     private ExtractedOntology extractOntology(final Long userId, final Long projectId, final RDFFormat rdfFormat)
         throws IOException, InterruptedException
     {
-        final var ontologyTag = uniqueUriIdHandler.uniquerUriTag(userId, projectId);
-        final var projectLabel = PROJECT_LABEL_ROOT + ontologyTag;
-        final var projectNodesCount = classNodeNeo4jDriverRepository.countNodeIdsByPropertiesAndLabels(List.of(projectLabel), List.of());
-
         final var extractedOntologyBuilder = ExtractedOntology.builder()
             .withRdfFormat(rdfFormat);
 
         final var exportId = UUID.randomUUID();
 
-        if (projectNodesCount <= MAX_NODE_COUNT)
-        {
-            final var serializedRDFGraph = neo4JGraphSerializer.serializeNeo4jGraphToRDF(projectLabel);
-            saveToFile(exportId, serializedRDFGraph, rdfFormat);
-            extractedOntologyBuilder.withExportId(exportId);
-            extractedOntologyBuilder.withAlreadyUntagged(false);
-        }
-        else
-        {
-            // TODO take care of bigger files
-            // TODO: https://stackoverflow.com/questions/57289877/how-to-paginate-results-of-cypher-neo4j
-            /*saveToBigFile(exportId, projectId, userId, rdfFormat);
-            extractedOntologyBuilder.withExportId(exportId);
-            extractedOntologyBuilder.withAlreadyUntagged(true);*/
+        final var extractedFile = extractToBigFile(exportId, projectId, userId, rdfFormat);
+        final var exportedFile = exportToBigFile(extractedFile, exportId, rdfFormat);
 
-            extractToBigFile(exportId, projectId, userId, rdfFormat);
-            final var exportedFile = exportToBigFile(exportId, rdfFormat);
-            extractedOntologyBuilder.withExportId(exportId);
-            extractedOntologyBuilder.withAlreadyUntagged(true);
-            extractedOntologyBuilder.withExtractedFile(exportedFile.toFile());
-        }
+        extractedOntologyBuilder.withExportId(exportId);
+        extractedOntologyBuilder.withExtractedFile(exportedFile.toFile());
 
         return extractedOntologyBuilder.build();
     }
 
-    private ExportOntologyResult exportOntology(final Long userId, final Long projectId, final ExtractedOntology extractedOntology)
+    private ExportOntologyResult prepareCompressedFile(final ExtractedOntology extractedOntology)
     {
-        final var ontologyTag = uniqueUriIdHandler.uniquerUriTag(userId, projectId);
         final var exportId = extractedOntology.exportId();
         final var rdfFormat = extractedOntology.rdfFormat();
 
         try
         {
-            final var extractedOntologyFile = new File(exportIdToExportedPath(exportId, rdfFormat).toString());
-            final var absolutePath = extractedOntologyFile.getCanonicalPath();
+            final var extractedFile = extractedOntology.extractedFile();
+            final var compressedFile = Paths.get(exportIdToPath(exportId, rdfFormat) + ".gz");
+            compressGzip(extractedFile.toPath(), compressedFile);
 
-            // TODO
-            if (extractedOntology.alreadyUntagged())
-            {
-                final var extractedFile = extractedOntology.extractedFile();
-                final var compressedFile = Paths.get(exportIdToPath(exportId, rdfFormat) + ".gz");
-                compressGzip(extractedFile.toPath(), compressedFile);
-
-                workspaceManagementService.clearWorkspace(extractedFile.getName());
-
-                return ExportOntologyResult.builder()
-                    .withExportId(exportId)
-                    .withExportedOntologyFile(compressedFile.toFile())
-                    .withInputStream(null)
-                    .build();
-            }
-
-            // TODO
-            // final var namespaces = prefixNodeService.findAll(projectId).map(PrefixMapping::prefixToUri).orElse(Map.of());
-            final var namespaces = Map.of(
-                "dc", "http://purl.org/dc/elements/1.1/",
-                "obo", "http://purl.obolibrary.org/obo/",
-                "owl", "http://www.w3.org/2002/07/owl#",
-                "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                "doid", "http://purl.obolibrary.org/obo/doid#",
-                "rdfs", "http://www.w3.org/2000/01/rdf-schema#",
-                "skos", "http://www.w3.org/2004/02/skos/core#",
-                "terms", "http://purl.org/dc/terms/",
-                "oboInOwl", "http://www.geneontology.org/formats/oboInOwl#"
-            );
-            final var exportedOntologyFile = rdfExporter.prepareRdfFileForExport(Path.of(absolutePath), ontologyTag, rdfFormat, namespaces);
-
-            final var inputStream = new BufferedInputStream(new FileInputStream(exportedOntologyFile));
+            workspaceManagementService.clearWorkspace(extractedFile.getName());
 
             return ExportOntologyResult.builder()
                 .withExportId(exportId)
-                .withInputStream(inputStream)
-                .withExportedOntologyFile(exportedOntologyFile)
+                .withExportedOntologyFile(compressedFile.toFile())
+                .withInputStream(null)
                 .build();
+
         }
         catch (final IOException ioException)
         {
             throw new IllegalStateException(ioException.getMessage());
         }
-    }
-
-    private void saveToFile(final UUID exportId, final String serializedGraph, final RDFFormat rdfFormat) throws IOException
-    {
-        final var filePath = exportIdToPath(exportId, rdfFormat);
-
-        final var fileWriter = new FileWriter(filePath.toFile().getAbsolutePath());
-        fileWriter.write(serializedGraph);
-        fileWriter.close();
-    }
-
-    private void saveToBigFile(final UUID exportId, final long projectId, final long userId, final RDFFormat rdfFormat) throws IOException
-    {
-        final var exportFile = exportIdToPath(exportId, rdfFormat).toFile();
-
-        final var projectTag = uniqueUriIdHandler.uniquerUriTag(userId, projectId);
-        final var projectLabel = List.of(uriUniquenessHandler.getClassNodeLabel(projectTag));
-
-        final var nodeCount = classNodeNeo4jDriverRepository.countNodeIdsByPropertiesAndLabels(projectLabel, List.of());
-        final int pageCount = (int) (nodeCount / BATCH_SIZE) + 1;
-
-        final var outputStream = new BufferedOutputStream(new FileOutputStream(exportFile));
-        final var rdfWriter = Rio.createWriter(rdfFormat, outputStream);
-        final var streamExportHandler = new RDFStreamExportHandler(rdfWriter, uriRemoveUniquenessHandler);
-        final var counter = new ArrayList<>(List.of(0));
-
-        streamExportHandler.startRDF();
-        IntStream.range(0, pageCount).forEach(batchIndex -> {
-            final var page = Pageable.ofSize(BATCH_SIZE).withPage(batchIndex);
-            final var statements = exportRepository.exportAttributes(projectId, userId, page);
-
-            log.info("written statements : {}", statements.size());
-            statements.forEach(streamExportHandler::handleStatement);
-            counter.set(0, counter.get(0) + statements.size());
-        });
-        log.info("exported : " + counter.get(0));
-/*
-        IntStream.range(0, pageCount).forEach(batchIndex -> {
-            final var page = Pageable.ofSize(BATCH_SIZE).withPage(batchIndex);
-            final var statements = exportRepository.exportRelationships(projectId, userId, page);
-
-            log.info("written statements : {}", statements.size());
-            statements.forEach(streamExportHandler::handleStatement);
-            counter.set(0, counter.get(0) + statements.size());
-        });
-        log.info("exported : " + counter.get(0));*/
-
-        streamExportHandler.endRDF();
-        outputStream.close();
     }
 
     private Path extractToBigFile(final UUID exportId, final long projectId, final long userId, final RDFFormat rdfFormat)
@@ -296,15 +173,16 @@ class ExportOntologyServiceImpl implements ExportOntologyService
         return neo4jBigGraphSerializer.serializeBigGraph(exportId, projectLabel, rdfFormat);
     }
 
-    private Path exportToBigFile(final UUID exportId, final RDFFormat rdfFormat) throws IOException
+    private Path exportToBigFile(final Path extractedFile, final UUID exportId, final RDFFormat rdfFormat) throws IOException
     {
-        final var extractedFile = exportIdToPath(exportId, rdfFormat);
         final var outputFile = exportIdToExportedPath(exportId, rdfFormat);
 
         final var input = new FileInputStream(extractedFile.toFile());
         final var output = new FileOutputStream(outputFile.toFile());
 
         final var writer = Rio.createWriter(rdfFormat, output);
+
+        // TODO handle namespaces
         final var exportHandler = new RDFStreamExportHandler(writer, uriRemoveUniquenessHandler);
 
         final var parser = Rio.createParser(rdfFormat);
